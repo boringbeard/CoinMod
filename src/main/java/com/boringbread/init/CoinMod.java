2a260e8e5c8a226b7de88f0a35a2323eda8e61a7
boringbread <79241012+boringbeard@users.noreply.github.com>
2021-07-20 23:27:39 -0500

    changed version

--- src/main/java/com/boringbread/init/CoinMod.java
+++ src/main/java/com/boringbread/init/CoinMod.java
@@ -17,7 +17,7 @@ import org.apache.logging.log4j.Logger;
 public class CoinMod {
     public static final String MOD_ID = "coinmod";
     public static final String NAME = "Boring Coin Mod";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     @Instance(CoinMod.MOD_ID)
     public static CoinMod instance;

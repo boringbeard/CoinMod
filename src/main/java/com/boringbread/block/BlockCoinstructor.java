5b3fea65a78bd032e44b4a4d9bb4b55915ad37a6
boringbread <79241012+boringbeard@users.noreply.github.com>
2022-06-21 16:57:58 -0500

    renamed some stuff and started to add the coinstructor

---
+++ src/main/java/com/boringbread/block/BlockCoinstructor.java
@@ -0,0 +1,13 @@
+package com.boringbread.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+
+public class BlockCoinstructor extends Block
+{
+    public BlockCoinstructor(Material blockMaterialIn, MapColor blockMapColorIn)
+    {
+        super(blockMaterialIn, blockMapColorIn);
+    }
+}

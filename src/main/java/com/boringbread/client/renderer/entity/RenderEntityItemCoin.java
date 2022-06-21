85234930d1fc07a8181993557e8d570dc7f48de2
boringbread <79241012+boringbeard@users.noreply.github.com>
2022-06-20 20:51:37 -0500

    coin now lays flat on ground with correct side facing up after flipping, coin also correctly follows the direction player flipped it from.

--- src/main/java/com/boringbread/client/renderer/entity/RenderEntityItemCoin.java
+++ src/main/java/com/boringbread/client/renderer/entity/RenderEntityItemCoin.java
@@ -11,7 +11,6 @@ import net.minecraft.client.renderer.entity.RenderEntityItem;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.MathHelper;
 
 public class RenderEntityItemCoin<E extends EntityItemCoin> extends RenderEntityItem
 {
@@ -25,63 +24,64 @@ public class RenderEntityItemCoin<E extends EntityItemCoin> extends RenderEntity
     @Override
     public void doRender(EntityItem entity, double x, double y, double z, float entityYaw, float partialTicks)
     {
-        if (!entity.onGround)
+        ItemStack stack = entity.getItem();
+
+        if (this.bindEntityTexture(entity))
         {
-            ItemStack stack = entity.getItem();
+            this.renderManager.renderEngine.getTexture(getEntityTexture(entity)).setBlurMipmap(false, false);
+        }
 
-            if (this.bindEntityTexture(entity))
-            {
-                this.renderManager.renderEngine.getTexture(getEntityTexture(entity)).setBlurMipmap(false, false);
-            }
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.alphaFunc(516, 0.1F);
+        GlStateManager.enableBlend();
+        RenderHelper.enableStandardItemLighting();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.pushMatrix();
 
-            GlStateManager.enableRescaleNormal();
-            GlStateManager.alphaFunc(516, 0.1F);
-            GlStateManager.enableBlend();
-            RenderHelper.enableStandardItemLighting();
-            GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-            GlStateManager.pushMatrix();
+        IBakedModel model = itemRenderer.getItemModelWithOverrides(stack, entity.world, null);
 
-            IBakedModel model = itemRenderer.getItemModelWithOverrides(stack, entity.world, null);
+        GlStateManager.translate((float) x, (float) y, (float) z);
 
-            GlStateManager.translate((float) x, (float) y + 0.25F, (float) z);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        if (this.renderOutlines)
+        {
+            GlStateManager.enableColorMaterial();
+            GlStateManager.enableOutlineMode(this.getTeamColor(entity));
+        }
 
-            if (this.renderOutlines)
-            {
-                GlStateManager.enableColorMaterial();
-                GlStateManager.enableOutlineMode(this.getTeamColor(entity));
-            }
+        GlStateManager.pushMatrix();
+        GlStateManager.rotate(entityYaw, 0.0F, -1.0F, 0.0F);
+        IBakedModel transformedModel = net.minecraftforge.client.ForgeHooksClient.handleCameraTransforms(model, ItemCameraTransforms.TransformType.GROUND, false);
 
-            GlStateManager.pushMatrix();
-            IBakedModel transformedModel = net.minecraftforge.client.ForgeHooksClient.handleCameraTransforms(model, ItemCameraTransforms.TransformType.GROUND, false);
-
+        if(!entity.onGround)
+        {
             float flip = ((float) entity.getAge() + partialTicks) * 90;
             GlStateManager.rotate(flip, 1.0F, 0.0F, 0.0F);
-
-            this.itemRenderer.renderItem(stack, transformedModel);
-            GlStateManager.popMatrix();
-            GlStateManager.translate(0.0F, 0.0F, 0.09375F);
-
-            if (this.renderOutlines)
-            {
-                GlStateManager.disableOutlineMode();
-                GlStateManager.disableColorMaterial();
-            }
-
-            GlStateManager.popMatrix();
-            GlStateManager.disableRescaleNormal();
-            GlStateManager.disableBlend();
-            this.bindEntityTexture(entity);
-
-            if (!this.renderOutlines)
-            {
-                this.renderName(entity, x, y, z);
-            }
         }
         else
         {
-            super.doRender(entity, x, y, z, entityYaw, partialTicks);
+            GlStateManager.rotate(-90.0F, 1.0F, 0.0F, 0.0F);
+        }
+
+        this.itemRenderer.renderItem(stack, transformedModel);
+        GlStateManager.popMatrix();
+        GlStateManager.translate(0.0F, 0.0F, 0.09375F);
+
+        if (this.renderOutlines)
+        {
+            GlStateManager.disableOutlineMode();
+            GlStateManager.disableColorMaterial();
+        }
+
+        GlStateManager.popMatrix();
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.disableBlend();
+        this.bindEntityTexture(entity);
+
+        if (!this.renderOutlines)
+        {
+            this.renderName(entity, x, y, z);
         }
     }
 }
